e673302274c2fbee083b721530c164f6ccf2695e
Tafarathedev <tafaratakaiza19@gmail.com>
2023-02-12 04:12:03 -0800

    first

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "url";
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "/@": path.resolve(__dirname, "./src"),
+      "/@": resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("src", import.meta.url))
     },
   }, build:{
     rollupOptions:{
